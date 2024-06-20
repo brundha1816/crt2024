3246ea2141de8de1f8a94e3ae098a709ac044b13
brundha1816 <dandaboyinabrundha@gmail.com>
2024-06-20 22:04:11 +0530

    Create Power

---
+++ Power.java
@@ -0,0 +1,15 @@
+Read m,n values calculate m power n
+
+import java.util.*;
+class Power
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int m,n;
+m=s.nextInt();
+n=s.nextInt();
+int c=(int)Math,pow(m,n);
+System.out.println(c);
+}
+}
